0d48f731e5a00fc2bb3c42b7c88dd3b34c305b7c
xupingwei <pingwei.xu@xiaoyeyun.com>
2017-05-04 12:22:05 +0800

    注释掉数据库配置

--- src/main/java/com/xaaolaf/BootdemoApplication.java
+++ src/main/java/com/xaaolaf/BootdemoApplication.java
@@ -3,11 +3,9 @@ package com.xaaolaf;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @EnableAutoConfiguration
-@EnableCaching
 public class BootdemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/xaaolaf/controller/HelloController.java
+++ src/main/java/com/xaaolaf/controller/HelloController.java
@@ -1,27 +1,24 @@
 package com.xaaolaf.controller;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by xupingwei on 2017/5/3.
  */
-@Controller
+@RestController
 public class HelloController {
 
-//    @RequestMapping(name = "index")
-//    public String index() {
-//        return "Hello world!";
-//    }
-
-
-    @RequestMapping(name = "/say")
-    public ModelAndView say() {
-
-        return new ModelAndView("Hello world!");
+    @RequestMapping(name = "index")
+    public String index() {
+        return "Hello world!";
     }
+
+
+//    @RequestMapping(name = "/say")
+//    public ModelAndView say() {
+//
+//        return new ModelAndView("Hello world!");
+//    }
 
 }
